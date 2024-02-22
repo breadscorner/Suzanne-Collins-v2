204e79254a778b0a18c183968708ae66083633c0
breadscorner <hello@brettgill.io>
2024-02-21 16:58:06 -0800

    add sc logo to header

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,14 +1,20 @@
-import { CMS_NAME } from "@/lib/constants";
 import Navbar from "./navbar";
-// import Image from "next/image";
+import Image from "next/image";
+import { HOME_OG_IMAGE_URL } from "@/lib/constants";
 
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-4 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
-        {CMS_NAME}
-      </h1>
-      {/* <Image src="/assets/suzanne-collins-black.jpg" alt="Suzanne Collins" width={200} height={200} /> */}
+      <div>
+        <a href="/">
+          <Image
+            src={HOME_OG_IMAGE_URL}
+            alt="Suzanne Collins"
+            width={300}
+            height={200}
+          />
+        </a>
+      </div>
       <div className="text-center md:text-left text-lg mt-5 md:pl-8">
         <Navbar />
       </div>
