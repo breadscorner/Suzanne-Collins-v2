ccd4f86bbaa270baf67c34db766b6d3cf20729c7
breadscorner <hello@brettgill.io>
2024-02-25 12:12:19 -0800

    fix images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+// next.config.js
 module.exports = {
-  reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'localhost.com',  
+      },
+    ], 
   },
-};
+}
